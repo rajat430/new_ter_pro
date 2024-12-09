9feced713aba24a13845b1cdbbfd402fff1b778e
rajatbhawsar <rajatbhawsar@KTRAININD.COM>
2024-12-09 16:45:12 +0530

    first commit

---
+++ storage/main.tf
@@ -0,0 +1,15 @@
+data "azurerm_resource_group" "example" {
+  name = var.rgName
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storagename
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ storage/variables.tf
@@ -0,0 +1,15 @@
+variable "rgName" {
+  default = "1-31cb7e34-playground-sandbox"
+  description = "This is rg name"
+  type = string
+}
+
+variable "location" {
+  default = "southcentralus"
+  type = string
+  description = "This is the location"
+}
+
+variable "storagename" {
+  type = string
+}
