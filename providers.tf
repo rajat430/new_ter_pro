cc0922b3369f1e553424c79c8ddd8eaf9e72e775
rajatbhawsar <rajatbhawsar@KTRAININD.COM>
2024-12-09 15:26:31 +0530

    first commit

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform { 
+    required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=4.9.0"
+    }
+  }
+  cloud { 
+    organization = "rajat430" 
+    workspaces { 
+      name = "ter_proj_dev_latest" 
+    } 
+  } 
+}
+
+provider "azurerm" {
+  features {}
+    resource_provider_registrations = "none"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rgName" {
+  default = "1-31cb7e34-playground-sandbox"
+  description = "This is rg name"
+  type = string
+}
+
+variable "location" {
+  default = "southcentralus"
+  type = string
+  description = "This is the location"
+}
